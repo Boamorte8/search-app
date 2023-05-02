a1225e793a5805f0c7840d7d2fd9490603252188
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-05-01 20:44:08 -0500

    feat: :sparkles: implement a filter of vegetables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'search-app';
+export class AppComponent implements OnInit {
+  private searchText$ = new BehaviorSubject<string>('');
+  currentSearchText = this.searchText$.asObservable();
+
+  searchText = '';
+  items = [
+    'Apple',
+    'Banana',
+    'Cherry',
+    'Durian',
+    'Elderberry',
+  ];
+  filteredItems: string[] = [];
+
+  ngOnInit(): void {
+    this.currentSearchText.subscribe((text) => {
+      this.searchText = text;
+      this.filteredItems = this.items.filter((item) =>
+        item.toLowerCase().includes(text.toLowerCase())
+      );
+    });
+  }
+
+  onSearch(value: string) {
+    this.searchText$.next(value);
+  }
 }
